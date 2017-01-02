fd935f7114ddd45dc63a4659c781fb990570dec1
Axel Schmidt <schmidta@mit.edu>
2017-01-02 16:29:24 -0500

    Forgot to include some files.

---
+++ include/constants.h
@@ -0,0 +1,11 @@
+#ifndef __CONSTANTS_H__
+#define __CONSTANTS_H__
+
+const double alpha=0.00729735253;
+
+// Units of GeV
+const double mP = 0.93827231;
+const double mN = 0.93956536;
+const double mD = 1.8756;
+
+#endif
