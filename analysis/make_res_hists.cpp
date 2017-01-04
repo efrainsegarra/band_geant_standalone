216472d2e5f37d5f08658e5f447caa181d2fe58e
Axel Schmidt <schmidta@mit.edu>
2017-01-04 16:53:39 -0500

    Added bin-migration histograms to the analysis program.

--- analysis/make_res_hists.cpp
+++ analysis/make_res_hists.cpp
@@ -8,10 +8,11 @@
 #include "TH1.h"
 #include "TVector3.h"
 
+#include "constants.h"
+
 using namespace std;
 
 double sq(double x){ return x*x;};
-const double mN=0.93956541;
 
 int main(int argc, char ** argv)
 {
@@ -27,13 +28,16 @@ int main(int argc, char ** argv)
   
   // Set memory addresses for tree
   double trueXp, trueWp, reconXp, reconWp;
-  double mom_e[3], mom_r[3], true_mom_r[3];
+  double mom_e[3], true_mom_e[3], mom_r[3], true_mom_r[3];
   t->SetBranchAddress("reconPe_x",&(mom_e[0]));
   t->SetBranchAddress("reconPe_y",&(mom_e[1]));
   t->SetBranchAddress("reconPe_z",&(mom_e[2]));
   t->SetBranchAddress("reconPr_x",&(mom_r[0]));
   t->SetBranchAddress("reconPr_y",&(mom_r[1]));
   t->SetBranchAddress("reconPr_z",&(mom_r[2]));
+  t->SetBranchAddress("truePe_x",&(true_mom_e[0]));
+  t->SetBranchAddress("truePe_y",&(true_mom_e[1]));
+  t->SetBranchAddress("truePe_z",&(true_mom_e[2]));
   t->SetBranchAddress("truePr_x",&(true_mom_r[0]));
   t->SetBranchAddress("truePr_y",&(true_mom_r[1]));
   t->SetBranchAddress("truePr_z",&(true_mom_r[2]));
@@ -46,6 +50,10 @@ int main(int argc, char ** argv)
   int nXpBins=20;
   int nWpBins=26;
   TH2D * prRes = new TH2D("prRes","Recon Q^2 > 2., W_prime > 1.8;p_r;delta p_r;Counts",20,.28,.68,80,-0.1,0.1);
+  TH2D * asRes = new TH2D("asRes","Recon Q^2 > 2., W_prime > 1.8;a_s;delta a_s;Counts",20,1.3,1.55,80,-0.2,0.2);
+  TH2D * reconXpAsSame = new TH2D("reconXpAsSame","Stayed in same bin;Recon x_p;Recon a_s;Counts",10,0.2,0.6,5,1.3,1.55);
+  TH2D * reconXpAsDiff = new TH2D("reconXpAsDiff","Originated in other bin;Recon x_p;Recon a_s;Counts",10,0.2,0.6,5,1.3,1.55);
+  TH2D * trueXpAsDiff = new TH2D("trueXpAsDiff","Left the bin;True x_p;True a_s;Counts",10,0.2,0.6,5,1.3,1.55);
   TH2D * xPrimeRes[5];
   TH2D * WPrimeRes[5];
   double minAlpha[5];
@@ -81,7 +89,7 @@ int main(int argc, char ** argv)
 
       double p_e = sqrt(sq(mom_e[0]) +sq(mom_e[1]) + sq(mom_e[2]));
       double cosTheta = mom_e[2]/p_e;
-      double QSq = 2.*(10.9)*p_e*(1.-cosTheta);
+      double QSq = 2.*E1*p_e*(1.-cosTheta);
       // Make kinematic cuts
       if ((QSq > 2.) && (reconWp > 1.8))
 	{
@@ -91,10 +99,29 @@ int main(int argc, char ** argv)
 	  prRes->Fill(rVec_true.Mag(),rVec.Mag() - rVec_true.Mag());
 
 	  // Reconstruct alpha_s
-	  double nu = 10.9 - p_e;
-	  TVector3 qVec(-mom_e[0],-mom_e[1],10.9-mom_e[2]);
+	  double nu = E1 - p_e;
+	  TVector3 qVec(-mom_e[0],-mom_e[1],E1-mom_e[2]);
 	  double E_r = sqrt(sq(mN) + rVec.Mag2());
 	  double alpha_s = (E_r - rVec.Dot(qVec)/qVec.Mag())/mN; 
+
+	  double true_pe = sqrt(sq(true_mom_e[0]) +sq(true_mom_e[1]) + sq(true_mom_e[2]));
+	  double true_nu = E1 - true_pe;
+	  TVector3 true_qVec(-true_mom_e[0],-true_mom_e[1],E1-true_mom_e[2]);
+	  double true_E_r = sqrt(sq(mN) + rVec_true.Mag2());
+	  double true_alpha_s = (true_E_r - rVec_true.Dot(true_qVec)/true_qVec.Mag())/mN;
+
+	  asRes->Fill(true_alpha_s,alpha_s-true_alpha_s);
+
+	  // Determine if there is bin migration
+	  if (reconXpAsSame->FindBin(trueXp,true_alpha_s) == reconXpAsSame->FindBin(reconXp,alpha_s))
+	    {
+	      reconXpAsSame->Fill(reconXp,alpha_s);
+	    }
+	  else
+	    {
+	      reconXpAsDiff->Fill(reconXp,alpha_s);
+	      trueXpAsDiff->Fill(trueXp,true_alpha_s);
+	    }
 
 	  // Figure out what bin
 	  int aBin = floor((alpha_s - minAlpha[0])/0.05);
@@ -113,6 +140,10 @@ int main(int argc, char ** argv)
   TFile *outRoot = new TFile(argv[2],"RECREATE");
 
   prRes->Write();
+  asRes->Write();
+  reconXpAsSame->Write();
+  reconXpAsDiff->Write();
+  trueXpAsDiff->Write();
   for (int aBin=0 ; aBin <5 ; aBin++)
     {
       xPrimeRes[aBin]->Write();
