2bfb527b680954f028a2d23dc539ec7720b803d6
Efrain Segarra <efrainsegarra@Efrains-MBP.hsd1.ma.comcast.net>
2017-07-06 14:02:55 -0400

    Edited makeHist file to do peak-to-tail ratio of neutron flux @ the face of the detector for the full geometry geant setup

--- analysis/makeHists.cpp
+++ analysis/makeHists.cpp
@@ -20,81 +20,64 @@ double sq(double x){ return x*x;};
 
 int main(int argc, char ** argv)
 {
-  if (argc != 7){
+  if (argc != 3){
       cerr << "Wrong number of arguments. Instead use\n"
-	   << "\tmakeHists /path/to/reconstructions_noGeo/file \n"
-     << "\t /path/to/reconstructions_fullGeo/file \n"
-     << "\t /path/to/output/file firstBinVal secondBinVal numBins\n";
+	   << "\tmakeHists /path/to/reconstructions_fullGeo/file \n"
+     << "\t /path/to/output/file\n";
       exit(-1);
   }
 
-  TFile * inFile1 = new TFile(argv[1]);
-  TFile * inFile2 = new TFile(argv[2]);
-  TFile * outFile = new TFile(argv[3],"RECREATE");
-  TTree * inTree1 = (TTree*)inFile1->Get("ReconTree");
-  TTree * inTree2 = (TTree*)inFile2->Get("ReconTree");
+  TFile * inFile = new TFile(argv[1]);
+  TFile * outFile = new TFile(argv[2],"RECREATE");
+  TTree * inTree = (TTree*)inFile->Get("ReconTree");
 
-  BAND_Event * reconEvent_noGeo = NULL;
-  BAND_Event * reconEvent_fullGeo = NULL;
-  inTree1-> SetBranchAddress("band",&reconEvent_noGeo);
-  inTree2-> SetBranchAddress("band",&reconEvent_fullGeo);
+  BAND_Event * reconEvent = NULL;
+  inTree-> SetBranchAddress("band",&reconEvent);
 
-  double firstBinVal = atof(argv[4]);
-  double lastBinVal  = atof(argv[5]);
-  double numBins     = atof(argv[6]);
+  int firstBinVal = 100;
+  int lastBinVal  = 600;
+  int numBins     = 500;
 
   // Prep histograms
-  TH1D * pr_noGeo = new TH1D("pr_noGeo","Pr Magnitude at face of BAND, no geometry",numBins,firstBinVal,lastBinVal); 
-  TH1D * pr_fullGeo = new TH1D("pr_fullGeo","Pr Magnitude at face of BAND, full geometry",numBins,firstBinVal-numBins/2,lastBinVal-numBins/2);
+  TH1D * pr_fullGeo = new TH1D("pr_fullGeo","Pr Magnitude at face of BAND, full geometry",numBins,firstBinVal,lastBinVal);
   double pr,pr_peak;
   int numEvents;
 
-  // Loop over events
-  numEvents = inTree1->GetEntries();
+  numEvents = inTree->GetEntries();
   for(int i =0; i<numEvents; i++){
     
     //if(i%100000==0)cerr << "Working on event " << i << "\n";
-    inTree1->GetEntry(i);
+    inTree->GetEntry(i);
 
     //cout << "Event " << i << endl;
 
-    for(int j=0; j<reconEvent_noGeo->hits.size(); j++){
+    for(int j=0; j<reconEvent->hits.size(); j++){
       
-      pr = reconEvent_noGeo->hits[j].mom.Mag();
-      pr_noGeo->Fill(pr);
-    
-    }
-  }
-  pr_peak = firstBinVal+(pr_noGeo->GetMaximumBin())*(lastBinVal-firstBinVal)/numBins;
-
-  numEvents = inTree2->GetEntries();
-  for(int i =0; i<numEvents; i++){
-    
-    //if(i%100000==0)cerr << "Working on event " << i << "\n";
-    inTree2->GetEntry(i);
-
-    //cout << "Event " << i << endl;
-
-    for(int j=0; j<reconEvent_fullGeo->hits.size(); j++){
-      
-      pr = reconEvent_fullGeo->hits[j].mom.Mag();
-
-      if (abs(pr - pr_peak) < 1.) continue;
-
+      pr = reconEvent->hits[j].mom.Mag();
       pr_fullGeo->Fill(pr);
     
     }
   }
+  // This is the peak momentum value -- i.e. the momentum that we shot at the detector
+  pr_peak = firstBinVal + (pr_fullGeo->GetMaximumBin())*(lastBinVal-firstBinVal)/numBins;
 
   double peak, tail;
-  peak = pr_noGeo->GetMaximum();
-  tail = pr_fullGeo->GetEntries();
+  int maxBin, minBin;
+
+  // Defines the peak as +/- 1.5% of the delta peak value
+  maxBin = pr_fullGeo->GetXaxis()->FindBin(pr_peak*1.015);
+  minBin = pr_fullGeo->GetXaxis()->FindBin(pr_peak*0.985);
+  peak = pr_fullGeo->Integral(minBin,maxBin);
+
+  // Defines the tail as all events up to 100 MeV/c
+  maxBin = pr_fullGeo->GetXaxis()->FindBin(pr_peak*0.985);
+  minBin = pr_fullGeo->GetXaxis()->FindBin(100);
+  tail = pr_fullGeo->Integral(minBin,maxBin);
+  
   cout << "Neutron Momentum [MeV/c]: " << pr_peak << "\nPeak-to-tail ratio: " << peak/tail << endl;
 
   pr_fullGeo->Write();
-  pr_noGeo->Write();
 
   outFile->Close();  
-  inFile1->Close();
-  inFile2->Close();
+  inFile->Close();
 }
