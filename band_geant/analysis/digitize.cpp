bd0ea7c5223abdfc80c3faab0f895c6677b975fb
Efrain Segarra <efrainsegarra@VPN-18-101-16-155.MIT.EDU>
2017-07-30 23:18:10 +0300

    Reverted back to full BAND array. Need to systematically study efficiency.
    Look at 1 bar,
    Look at 1 thick bar
    Look at 5 bars
    Look at full BAND array.
    
    The alst two should be almost similar if I do straight shots. Except BAND array should be more efficient because more coverage for hits.
    
    The 2nd should possibly be most efficient because it has large thickness, and so there is less segmentation, and more available area for energy deposition

--- band_geant/analysis/digitize.cpp
+++ band_geant/analysis/digitize.cpp
@@ -23,7 +23,7 @@ const double barCrossSection = 7.4;// in cm
 const double BAND_Zoffset = -262; // in cm
 const int BAND_numLayers = 5;
 const int numBANDbars = 24; // bars in a single layer of the BAND array
-const double BAND_Yoffset = barCrossSection * 5;
+const double BAND_Yoffset = barCrossSection * BAND_numLayers;
 
 // ********************************************************************************
 // Reconstruction options
