d41dae5f1d4af7974a6b41cbe9268aee6c8c2b50
Efrain Segarra <efrainsegarra@VPN-18-101-16-153.MIT.EDU>
2017-08-03 18:17:04 +0300

    Also edited Isotropic generator to only take BAND events

--- isotropic_CLAS12.cpp
+++ isotropic_CLAS12.cpp
@@ -14,11 +14,6 @@
 using namespace std;
 
 // Foam ranges
-//const double min_theta_e = 5.*M_PI/180.;
-//const double max_theta_e = 35.*M_PI/180.;
-//const double min_p_e = 2.;
-//const double max_p_e = 8.;
-
 const double min_theta_r = 170.*M_PI/180.;//154.3*M_PI/180.;
 const double max_theta_r = 170.*M_PI/180.;//175.8*M_PI/180.;
 const double min_p_r =0.55;
@@ -26,8 +21,64 @@ const double max_p_r =0.55;
 const double min_phi_r=M_PI/2.;
 const double max_phi_r=M_PI/2.;
 
-//const double min_phi_er=0.;
-//const double max_phi_er=2.*M_PI;
+int checkBAND(double theta_r,double phi_r){
+  // ONLY SAVE THE EVENTS THAT WILL MAKE IT INTO BAND
+  double numbars,prevbars;
+  double max_x, min_x,max_x_2,min_x_2;
+  double max_y = 7.4*13;
+  double min_y = -7.4*5;
+
+  double r = -262. / cos(theta_r) ;
+  double x_traj = r * cos(phi_r) * sin(theta_r) ;
+  double y_traj = r * sin(phi_r) * sin(theta_r) ;
+  if ((y_traj > max_y) || (y_traj < min_y)) return -1;
+
+  // first group
+  prevbars = 0;
+  numbars = 2;
+  if ((y_traj > min_y + prevbars*7.4) && (y_traj < min_y + (numbars+prevbars)*7.4)){
+    max_x = 2018.35 / 10 / 2;
+    min_x = -max_x;
+    if ((x_traj > max_x) || (x_traj < min_x)) return -1;
+  }
+  // second group
+  prevbars += numbars;
+  numbars = 6;
+  if ((y_traj > min_y + prevbars*7.4) && (y_traj < min_y + (numbars+prevbars)*7.4)){
+    max_x = (509.17/10 + 500/10);
+    min_x = (509.17/10 );
+    max_x_2 = -(509.17/10 );
+    min_x_2 = -(509.17/10 + 500/10);
+    if ((x_traj > max_x) || (x_traj < min_x_2)) return -1;
+    if (( x_traj < min_x ) && (x_traj > max_x_2) ) return -1;
+  }
+  // third group
+  prevbars += numbars;
+  numbars = 4;
+  if ((y_traj > min_y + prevbars*7.4) && (y_traj < min_y + (numbars+prevbars)*7.4)){
+    max_x = 2018.35 / 10 / 2;
+    min_x = -max_x;
+    if ((x_traj > max_x) || (x_traj < min_x)) return -1;
+  }
+  // fourth group
+  prevbars += numbars;
+  numbars = 3;
+  if ((y_traj > min_y + prevbars*7.4) && (y_traj < min_y + (numbars+prevbars)*7.4)){
+    max_x = 1946.53 / 10 / 2;
+    min_x = -max_x;
+    if ((x_traj > max_x) || (x_traj < min_x)) return -1;
+  }
+  // five group
+  prevbars += numbars;
+  numbars = 3;
+  if ((y_traj > min_y + prevbars*7.4) && (y_traj < min_y + (numbars+prevbars)*7.4)){
+    max_x = 1634.58 / 10 / 2;
+    min_x = -max_x;
+    if ((x_traj > max_x) || (x_traj < min_x)) return -1;
+  }
+  cout << x_traj << " " << y_traj << "\n";
+  return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -64,7 +115,9 @@ int main(int argc, char *argv[])
       double p_r = min_p_r + (max_p_r - min_p_r) * rand->Rndm();
       double phi_r = min_phi_r + (max_phi_r - min_phi_r) * rand->Rndm();
 
-
+      int pass = checkBAND(theta_r,phi_r);
+      if (pass < 0) continue;
+      
       // Write to tree
       thisEvent->particles.clear();
       //Gen_Particle electron;
