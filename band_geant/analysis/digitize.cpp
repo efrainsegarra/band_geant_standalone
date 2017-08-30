0daa6b2617661166062f1d0416c810a24087b4a0
Efrain Segarra <efrainsegarra@Efrains-MacBook-Pro.local>
2017-08-30 10:37:54 -0400

    Fixed the current files

--- band_geant/analysis/digitize.cpp
+++ band_geant/analysis/digitize.cpp
@@ -82,12 +82,8 @@ int main(int argc, char** argv){
 			// in MeVee
 			// this conversion from http://shop-pdp.net/efhtml/NIM_151_1978_445-450_Madey.pdf
 			double trueE = geantEvent->hits[j].E_dep; // in MeV
-<<<<<<< HEAD
 			double trueE_MeVee = 0.83 * trueE - 2.82 * ( 1 - exp( 0.25 * ( pow(trueE,0.93)) ) );
-=======
-			double trueE_MeVee = 0.95 * trueE - 8.0 * ( 1 - exp( 0.10 * ( pow(trueE,0.90)) ) );
 
->>>>>>> c28c0660d0ce5eddd5f0476c3a457ed5d9fb4647
 			if(abs(trueE_MeVee)<threshold) continue;
 
 			// If hit above threshold, save the hit time to
